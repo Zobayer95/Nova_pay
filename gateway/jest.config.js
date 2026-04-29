1871243a7bc9f2171754f627be56fb8e818d6ca0
Zobayer Abdullah <zobayer.abdullah9999@gmail.com>
2026-04-29 15:31:36 +0600

    Update jest.config.js

--- gateway/jest.config.js
+++ gateway/jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node', 
   roots: ['<rootDir>/src'],
-  testMatch: ['**/*.test.ts'],
+  testMatch: ['**/*.test.ts'], 
   moduleFileExtensions: ['ts', 'js', 'json'],
 };
